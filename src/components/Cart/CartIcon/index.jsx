cad760fe57aeff13e283df63896e766dd6c3f0d1
mahdi <109016246+mahdi81007@users.noreply.github.com>
2025-05-05 17:10:24 +0330

    feat : add cartIcon component  part 1

---
+++ src/components/Cart/CartIcon/index.jsx
@@ -0,0 +1,10 @@
+
+function Index(icon) {
+    return (
+        <div className={'rounded-full size-[68px] p-[19px] bg-primary'}>
+            <i className={`${icon} text-16 text-white text-center`}></i>
+        </div>
+    );
+}
+
+export default Index;
