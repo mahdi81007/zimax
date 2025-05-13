218e3e7571cc2ef4775d11a523056f7eafae6234
mahdi <109016246+mahdi81007@users.noreply.github.com>
2025-05-13 21:09:52 +0330

    fix : style

--- src/components/Animation/index.jsx
+++ src/components/Animation/index.jsx
@@ -25,21 +25,21 @@ function Index() {
         <div
             className={'relative  bg-[radial-gradient(ellipse_71.17%_71.17%_at_50.00%_50.00%,_var(--Background-Primary-filled,_#228BE6)_0%,_#0B2E4D_100%)] overflow-hidden h-[100vh]'}>
             {!showSectionTwo ? (
-                <Container className={'text-white pt-[100px] text-center align-center'}>
+                <Container className={'text-white pt-[200px] text-center align-center'}>
                     <img
                         src={logoImg.src}
                         alt={'logo'}
                         style={{margin: '0 auto'}}
-                        className={'w-[182px] h-[53px] animate-fade delay-300'}
+                        className={'w-[182px]  h-[53px] animate-fade delay-300'}
                     />
                     <h1 className={'mt-[29px] animate-fade delay-600'}>
                         For Your Own Decentralized <b>Business</b>
                     </h1>
                     <div className={'mt-[12px] '}>
-                        <span className={'animate-fade  delay-900'}>Built on <b>web3</b></span>
+                        <span className={'animate-fade  delay-900'}>Built on <b> web3  </b></span>
                         <span className={'animate-fade delay-1200'}>Powered by <b>You</b></span>
                     </div>
-                    <p className={'mt-[54px]  animate-fade delay-1500'}>
+                    <p className={'mt-[54px] leading-loos  animate-fade delay-1500'}>
                         ZiMax is not just secure and transparent but also has <b>" NO "</b>
                         intermediaries. ZiMax used <b>" NO "</b> middlemen/intermediary admin control. It is based on
                         smart-contract.
