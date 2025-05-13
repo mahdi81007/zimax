7d7b04b73bac41d8761947ed921e6782ad40eafd
mahdi <109016246+mahdi81007@users.noreply.github.com>
2025-05-13 21:08:19 +0330

    fix : style cartIcon

--- src/components/Cart/CartIcon/index.jsx
+++ src/components/Cart/CartIcon/index.jsx
@@ -1,8 +1,8 @@
 
-function Index(icon) {
+function Index({icon}) {
     return (
-        <div className={'rounded-full size-[68px] p-[19px] bg-primary'}>
-            <i className={`${icon} text-16 text-white text-center`}></i>
+        <div className={'rounded-full m-auto max-w-[68px]  p-[19px] bg-blue-500 flex items-center justify-center'}>
+            <i className={`${icon} text-[30px] text-white text-center`}></i>
         </div>
     );
 }
