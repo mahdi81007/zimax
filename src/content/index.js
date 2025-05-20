cfbd5657316615f637aed3db23fbe4dd4c13689b
mahdi <109016246+mahdi81007@users.noreply.github.com>
2025-05-20 10:12:44 +0330

    feat : add cart content

---
+++ src/content/index.js
@@ -0,0 +1,26 @@
+export const cartIconOne = [
+    {
+        title: 'Auto-Compounding',
+        icon: 'icon-card-coin',
+        content: 'Crypto’s Highest Paying Auto-Staking and Auto-Compounding Protocol with the greatest APY in the industry up to 0.000854329%. Interest rewards are compounded every 15 MINUTES for every BSC wallet holding any $ZMX tokens.'
+    }, {
+        title: 'Risk Insurance Fund',
+        icon: 'icon-command',
+        content: 'RIF ensures holders safety from sudden market crash, long-term stability and future growth of ZiMax DeFi and minimum risk of price reduction. Therefore, staking rewards, which are distributed every 15 MINUTES at a rate of 0.000854329% are backed by the RIF parameter, which ensures a high and stable interest rate for $ZMX token holders.'
+    }, {
+        title: 'icon-bank',
+        icon: 'icon-card-coin',
+        content: 'One of the most important departments in ZiMax DeFi is the treasury. The treasury is designed for additional financial support of RIF. In case of a strong price fall of token $ZMX, it will receive additional financing from this department. The treasury can be used for financing new products and projects that will be released on the basis of ZiMax DeFi.'
+    }, {
+        title: 'ZiMax Treasury',
+        icon: 'icon-money-tick',
+        content: 'Large trading amount increases the number of tokens burned, thus reducing circulating supply and maintained stability of ZiMax DeFi.\n' +
+            'This system has another important advantage. The fact that it is deflationary allows the $ZMX token to maintain a higher value.'
+    },
+]
+
+export const bonusCard = [
+    {time: '6 Months', apy :'33.25% APY',bonus :'Total Reward including 15%  Bonus'},
+    {time: '1 Year', apy :'76,10% APY',bonus :'Total Reward including 40% Bonus'},
+    {time: '2 Years', apy :'98,40% APY',bonus :'Total Reward including 60% Reward 16:09'},
+]
