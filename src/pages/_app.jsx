4d85a3a66a943b438826734a4e8425d13b3193ef
mahdi <109016246+mahdi81007@users.noreply.github.com>
2025-05-28 14:40:39 +0330

    feat : add theme

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,20 @@
-import "@/styles/globals.css";
-import {MantineProvider} from "@mantine/core";
+import '../styles/index.css';
+import '../styles/globals.css'
+import '@mantine/core/styles.css';
+import {createTheme, MantineProvider} from "@mantine/core";
 import Head from "@/components/Meta/Head";
 
+
 export default function App({Component, pageProps}) {
+    const theme = createTheme({
+        fontFamily: 'Poppins, sans-serif',
+        fontFamilyMonospace: 'Monaco, Courier, monospace',
+        headings: { fontFamily: 'Outfit, sans-serif' },
+    });
     return (
         <>
             <Head/>
-            <MantineProvider>
+            <MantineProvider theme={theme}>
                 <Component {...pageProps} />;
             </MantineProvider>
         </>
